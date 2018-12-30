69723e791ada635bfd827387b5a676f32e9ea238
shkamboj <iiitu16118@gmail.com>
2018-12-30 14:28:35 +0530

    initial code

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ var cookieParser = require('cookie-parser');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
-// var uri ='mongodb://shkamboj:qwerty123@localhost:27017/open_elec16?authSource=admin'; 
-
-var uri = 'mongodb://amit:amit123@ds237072.mlab.com:37072/quizapp';
-
- 
-mongoose.connect(uri);
-
 
 
 
