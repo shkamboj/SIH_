819b65e10114af051d7e1b3b85a5f4b95568cea3
shkamboj <iiitu16118@gmail.com>
2019-01-06 17:00:07 +0530

    added

--- index.js
+++ index.js
@@ -1,52 +1,97 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var cookieParser = require('cookie-parser');
+var
+  express = require('express'),
+  url = require('url'),
+  fs = require('fs'),
+  path = require('path'),
+  app = express(),
+  quizzer = require('node-quizzer'),
+  _ = require('underscore-node'),
+  getQuiz = function(method, req) {
+    var urlParts = url.parse(req.url, true),
+      query = urlParts.query,
+
+      // generate random quiz
+      quiz = quizzer[method]({
+        uname: query.fullname,
+        uemail: query.email,
+        name: query.quiz,
+        count: parseInt(query.count),
+        time: parseInt(query.time),
+        perc: parseInt(query.perc)
+      });
+
+    return quiz;
+  };
 
 
-var app = express();
-app.set('port', (process.env.PORT || 5000));
+app.get('/', function(req, res) {
+  var list = quizzer.getCategories();
+  console.log(list);
 
+  // load the index.html template
+  fs.readFile(__dirname + '/public/index.html', function(err, data) {
+    if(err) throw err;
 
-
-
-
-app.get('/',function(req,res){
-  res.render('main');
+    // populate it with templated questions from the node-quizzer module
+    var compiled = _.template(data.toString());
+    res.send(compiled({ availableQuizzes: list }));
+  });
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/quiz', function(req, res) {
+  var quiz = getQuiz('generate', req);
 
-app.set('view engine', 'ejs');
-app.set('views','./views');
+  // load the quiz.html template
+  fs.readFile(__dirname + '/public/quiz.html', function(err, data) {
+    if(err) throw err;
 
-var path = require('path');
-app.use('/static',express.static(__dirname + '/public'));
-
-const url = require('url');
-
-
-app.use(cookieParser());
-
-
-
-
- 
-
-
-
-app.set("view options", { layout: false } );
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:true}));
-
-
-
-
-
-
-
-
-
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'));
+    // populate it with templated questions from the node-quizzer module
+    var compiled = _.template(data.toString());
+    res.send(compiled({ quiz: quiz }));
+  });
 });
+
+app.get('/tokenize', function(req, res) {
+  var quiz = getQuiz('tokenize', req),
+    tokenUrl = req.protocol + '://' + req.get('host') + "/quiz/" + quiz.quid;
+
+  res.set('Content-Type', 'text/plain');
+  res.send(tokenUrl);
+});
+
+app.get('/quiz/:id', function(req, res) {
+  var quiz = quizzer.fromToken(req.params.id);
+
+  // load the quiz.html template
+  if(quiz) {
+    fs.readFile(__dirname + '/public/quiz.html', function(err, data) {
+      if(err) throw err;
+
+      // populate it with templated questions from the node-quizzer module
+      var compiled = _.template(data.toString());
+      res.send(compiled({ quiz: quiz }));
+    });
+  } else {
+    res.send("This token has expired!");
+  }
+})
+
+app.get('/review', function(req, res) {
+  var urlParts = url.parse(req.url, true),
+    query = urlParts.query,
+    results = quizzer.evaluate(query);
+
+  // load the review.html template
+  fs.readFile(__dirname + '/public/review.html', function(err, data) {
+    if(err) throw err;
+
+    // populate it with templated questions from the node-quizzer module
+    var compiled = _.template(data.toString());
+    res.send(compiled({ results: results }));
+  });
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
