d254c5cb338cf6997e65d399d5ef685065aa40bc
shkamboj <iiitu16118@gmail.com>
2019-03-02 11:26:40 +0530

    popups done

--- index.js
+++ index.js
@@ -14,10 +14,17 @@ const PORT = process.env.PORT || 5100;
 app.set('view engine', 'ejs');
 app.set('views','./views');
 
+var path = require('path');
+app.use('/static',express.static(__dirname + '/public'));
+
 
 
 app.get('/', function(req, res) {
   res.send('the losers');
+});
+
+app.get('/tender', function(req, res) {
+  res.render('tender');
 });
 
 
